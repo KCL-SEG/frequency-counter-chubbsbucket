3f48aa7314dd3ee70dd8c7edc8cef260ef42a66c
Chhavi Dhir <dchhavi762@gmail.com>
2023-10-09 02:05:41 +0100

    Solve frequencies problem

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
-    return frequencies
+    for item in items:
+        if not isinstance(item, str):
+            item = str(item)
+        if(frequencies.get(item)):
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1
+    else:
+        return frequencies
